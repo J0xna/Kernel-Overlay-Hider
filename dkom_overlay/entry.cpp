5a6488a157e1c2ca8353c5a8f93dfd767c57b332
jz02 <82886476+J0xna@users.noreply.github.com>
2024-01-24 19:53:23 +0200

    Update entry.cpp

--- dkom_overlay/entry.cpp
+++ dkom_overlay/entry.cpp
@@ -15,12 +15,6 @@ struct comms_t {
 };
 
 bool handle_overlay( comms_t* ptr ) {
-	/*
-	!! WARNING !!
-	This overlay dkom method will result in a blue screen of death if you try to close the host process / destroy window without restoring the old state.
-	!! WARNING !!
-	*/
-
 	if ( !ptr->window.handle ) {
 		log_e( "passed hwnd invalid...\n" );
 		return false;
@@ -47,55 +41,23 @@ bool handle_overlay( comms_t* ptr ) {
 		return false;
 	}
 
-	/* 
-	This dkom method is designed to work on Windows 10, 21H2 and the offsets will be outdated on many versions.
-	You might bluescreen if you don't do this check.
-
-	Run this check on same winver Virtual Machine to validate offsets and prevent BSOD.
-	This check should return log of the offsets if it finds instance->Next->Previous or instance->Previous->Next ( 2 matches ).
-
-	^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-	const auto w = reinterpret_cast< std::uintptr_t >( our_wnd );
-		for ( std::uint32_t i = 0; i < 0x100; i += 0x8 ) {
-			if ( MmIsAddressValid( reinterpret_cast< void* >( w + i ) ) ) {
-				auto w_2 = *( std::uintptr_t* )( w + i );
-				for ( std::uint32_t x = 0; x < 0x100; x += 0x8 ) {
-					if ( MmIsAddressValid( reinterpret_cast< void* >( w_2 + x ) ) ) {
-						if ( *( std::uintptr_t* )( w_2 + x ) == w ) {
-							log( "%llx | %llx\n", i, x );
-						}
-					}
-				}
-			}
-		}
-	^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-	*/
-
-	// Sanity check to avoid bsod.
+	// Sanity check to avoid Blue Screen of Death.
 	if ( our_wnd != our_wnd->prev->next || our_wnd != our_wnd->next->prev ) {
 		log_e( "TAG_WND structure outdated, check offsets with bruteforcer above!\n" );
 		return false;
 	}
 
-	/* 
-	This right here swaps the order of the list making it so our overlay handle will never be in the list. 
-	The handle disappears and our window will be invisible from enumerations ( Check on Process Hacker / Spy++ / EnumWindows callback )
-	This method also made it so that BattlEye shellcode emulator was unable to find the window and it's properties.
-	*/
 	if ( !( our_wnd->prev->next = our_wnd->next ) || !( our_wnd->next->prev = our_wnd->prev ) ) {
-		log_e( "something is incredibly fucked...\n" );
+		log_e( "something went really really wrong...\n" );
 		return false;
 	}
 
-	// Simple as that, we have achieved dkomed window, now up-to reader: you have to fix BSODs when you close the software owning the window handle. Don't add me to ask how to do it :=)
 	log_s( "overlay handled successfully!\n" );
 
 	return true;
 }
 
 std::int64_t callback( comms_t* a1, void* a2 ) {
-	// for this example i cba to make communication code enum, 1 is enough for the POC.
-
 	static comms_t buffer = { };
 	if ( ExGetPreviousMode( ) != UserMode || !util::memory_t::safe_copy( &buffer, a1, sizeof( comms_t )) || buffer.key != 0xCA ) {
 		return orig_callback( a1, a2 );
@@ -111,17 +73,15 @@ NTSTATUS entry( ) {
 	if ( !util::module_t::init( ) )
 		return STATUS_UNSUCCESSFUL;
 
-	//Forgive me for using a data pointer to communicate, the caller thread will 100% have a thread context of a win32 thread which is required to process the overlay with this method.
-
 	const auto ptr = rva( util::module_t::win32k->find_pattern( _( "\x48\x8B\x05\xFD\x6F\x05\x00\x48\x85\xC0" ), _( "xxxxxx?xxx" ) ), 7 );
 
 	if ( !ptr ) {
-		log_e( ".data ptr not found...\n" );
+		log_e( "ptr not found...\n" );
 		return STATUS_UNSUCCESSFUL;
 	}
 
 	if ( !( *reinterpret_cast< void** >( &orig_callback ) = _InterlockedExchangePointer( reinterpret_cast< void** >( ptr ), callback ) ) ){
-		log_e( "data ptr swap failed...\n" );
+		log_e( "swapping pointer failed...\n" );
 		return STATUS_UNSUCCESSFUL;
 	}
 
